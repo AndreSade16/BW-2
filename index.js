53b2376fc26ceaef5cf1658f7a112b0613c7bf49
Vals098 <valeria.farinosi@gmail.com>
2026-04-22 14:58:39 +0200

    add: artist logic

--- index.js
+++ index.js
@@ -19,19 +19,17 @@ window.addEventListener("DOMContentLoaded", function () {
     openButton.classList.add("d-none")
   })
 
-  // Logica popolamento artist carousel e album carousel
-
+  // Logica popolamento album carousel
+  
+  const albumUrl = "https://striveschool-api.herokuapp.com/api/deezer/album/"
   const albumIds = [
-    7714217, 7714220, 7714218, 313357207, 302127, 75621062, 119606, 92759,
-    144076, 1327607,
+    13475611, 75621062, 119606, 111114312, 217794942, 14560832, 103248,
+    132971892, 113728, 225204, 154910, 198908,
   ]
-
-  const albumMain = []
-
   const albumMain = []
 
   albumIds.forEach((id) => {
-    fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${id}`)
+    fetch(`${albumUrl}${id}`)
       .then((response) => {
         if (!response.ok) throw new Error("Errore fetch")
         return response.json()
@@ -45,34 +43,96 @@ window.addEventListener("DOMContentLoaded", function () {
       })
       .catch((err) => console.log("ERRORE:", err))
   })
-  
 
   const showAlbums = function (albums) {
     const slide1 = document.getElementById("album-slide-1")
     const slide2 = document.getElementById("album-slide-2")
-
-    if (!slide1 || !slide2) {
-      console.log("SLIDE NON TROVATE")
-      return
-    }
 
     slide1.innerHTML = ""
     slide2.innerHTML = ""
 
     albums.forEach((album, index) => {
       const card = `
-  <div class="col-6 col-md-4 col-lg-2">
-    <div class="card bg-dark text-light border-0 p-2">
+  <div class="col-6 col-md-3 col-lg-2">
+    <div class="card bg-dark text-light border-0 p-2" onclick="loadAlbumPage(${album.id})">
       <img src="${album.cover_medium}" class="img-fluid mb-2">
       <p class="mb-1">${album.title}</p>
       <small class="text-secondary">
-        ${album.artist?.name || "Unknown"}
+        ${album.artist?.name}
       </small>
     </div>
   </div>
 `
 
-      if (index < 5) {
+      if (index < 6) {
+        slide1.innerHTML += card
+      } else {
+        slide2.innerHTML += card
+      }
+    })
+  }
+
+  // Logica popolamento artisti carousel
+const artistUrl = "https://striveschool-api.herokuapp.com/api/deezer/artist/"
+  const artistIds = [
+    27, // Daft Punk
+    412, // Queen
+    384236, // Dua Lipa
+    13, // Eminem
+    246791, // Drake
+    4050205, // Billie Eilish
+    75798, // Imagine Dragons
+    755, // Bruno Mars
+    111114, // Travis Scott
+    1562681, // Olivia Rodrigo
+    757, // David Guetta
+    12178, // Arctic Monkeys
+  ]
+
+  const artistMain = []
+
+  artistIds.forEach((id) => {
+    fetch(`${artistUrl}${id}`)
+      .then((response) => {
+        if (!response.ok) throw new Error("Errore fetch")
+        return response.json()
+      })
+      .then((artist) => {
+        console.log(artist.id, artist.name)
+        artistMain.push(artist)
+
+        if (artistMain.length === artistIds.length) {
+          showArtists(artistMain)
+        }
+      })
+      .catch((err) => console.log("ERRORE:", err))
+  })
+
+  const showArtists = function (artists) {
+    const slide1 = document.getElementById("artist-slide-1")
+    const slide2 = document.getElementById("artist-slide-2")
+
+    slide1.innerHTML = ""
+    slide2.innerHTML = ""
+
+    artists.forEach((artist, index) => {
+      const card = `
+      <div class="col-6 col-md-3 col-lg-2">
+        <div class="card bg-dark text-light border-0 p-3 text-center"  onclick="loadArtistPage(${artist.id})">
+          
+          <img 
+            src="${artist.picture_medium}" 
+            class="rounded-circle mb-3"
+            style="width: 100px; height: 100px; object-fit: cover;"
+          >
+
+          <p class="mb-1">${artist.name}</p>
+          <small class="text-secondary">Artista</small>
+        </div>
+      </div>
+    `
+
+      if (index < 6) {
         slide1.innerHTML += card
       } else {
         slide2.innerHTML += card
