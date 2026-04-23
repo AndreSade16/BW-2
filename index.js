164e90411a47c89403b719b1e9665ad45feb1ccf
Vals098 <valeria.farinosi@gmail.com>
2026-04-23 08:38:30 +0200

    fix: undefined albums

--- index.js
+++ index.js
@@ -1,55 +1,57 @@
 window.addEventListener("DOMContentLoaded", function () {
   // Logica chiusura sidebar-right al click del bottone "X"
-  const closeButton = document.getElementById("closeSidebar");
-  const openButton = document.getElementById("openSidebar");
-  const sidebar = document.getElementById("sidebarRight");
+  const closeButton = document.getElementById("closeSidebar")
+  const openButton = document.getElementById("openSidebar")
+  const sidebar = document.getElementById("sidebarRight")
 
   // stato iniziale
-  openButton.classList.add("d-none");
+  openButton.classList.add("d-none")
 
   // CHIUDI
   closeButton.addEventListener("click", function () {
-    sidebar.classList.remove("d-lg-block");
-    openButton.classList.remove("d-none");
-  });
+    sidebar.classList.remove("d-lg-block")
+    openButton.classList.remove("d-none")
+  })
 
   // APRI
   openButton.addEventListener("click", function () {
-    sidebar.classList.add("d-lg-block");
-    openButton.classList.add("d-none");
-  });
+    sidebar.classList.add("d-lg-block")
+    openButton.classList.add("d-none")
+  })
 
   // Logica popolamento album carousel
 
-  const albumUrl = "https://striveschool-api.herokuapp.com/api/deezer/album/";
+  const albumUrl = "https://striveschool-api.herokuapp.com/api/deezer/album/"
   const albumIds = [
-    13475611, 75621062, 119606, 111114312, 217794942, 14560832, 103248,
-    132971892, 113728, 225204, 154910, 198908,
-  ];
-  const albumMain = [];
+    111114312, 75621062, 217794942, 113728, 103248, 198908, 70928652, 13475611,
+    119606, 154910, 302127, 75621062,
+  ]
+
+  const albumMain = []
 
   albumIds.forEach((id) => {
     fetch(`${albumUrl}${id}`)
       .then((response) => {
-        if (!response.ok) throw new Error("Errore fetch");
-        return response.json();
+        if (!response.ok) throw new Error("Errore fetch")
+        return response.json()
       })
       .then((album) => {
-        albumMain.push(album);
+        console.log(album.id, album.title)
+        albumMain.push(album)
 
         if (albumMain.length === albumIds.length) {
-          showAlbums(albumMain);
+          showAlbums(albumMain)
         }
       })
-      .catch((err) => console.log("ERRORE:", err));
-  });
+      .catch((err) => console.log("ERRORE:", err))
+  })
 
   const showAlbums = function (albums) {
-    const slide1 = document.getElementById("album-slide-1");
-    const slide2 = document.getElementById("album-slide-2");
+    const slide1 = document.getElementById("album-slide-1")
+    const slide2 = document.getElementById("album-slide-2")
 
-    slide1.innerHTML = "";
-    slide2.innerHTML = "";
+    slide1.innerHTML = ""
+    slide2.innerHTML = ""
 
     albums.forEach((album, index) => {
       const card = `
@@ -62,18 +64,18 @@ window.addEventListener("DOMContentLoaded", function () {
       </small>
     </div>
   </div>
-`;
+`
 
       if (index < 6) {
-        slide1.innerHTML += card;
+        slide1.innerHTML += card
       } else {
-        slide2.innerHTML += card;
+        slide2.innerHTML += card
       }
-    });
-  };
+    })
+  }
 
   // Logica popolamento artisti carousel
-  const artistUrl = "https://striveschool-api.herokuapp.com/api/deezer/artist/";
+  const artistUrl = "https://striveschool-api.herokuapp.com/api/deezer/artist/"
   const artistIds = [
     27, // Daft Punk
     412, // Queen
@@ -87,56 +89,51 @@ window.addEventListener("DOMContentLoaded", function () {
     1562681, // Olivia Rodrigo
     757, // David Guetta
     12178, // Arctic Monkeys
-  ];
+  ]
 
-  const artistMain = [];
+  const artistMain = []
 
   artistIds.forEach((id) => {
     fetch(artistUrl + id)
       .then((response) => {
-        if (!response.ok) throw new Error("Errore fetch");
-        return response.json();
+        if (!response.ok) throw new Error("Errore fetch")
+        return response.json()
       })
       .then((artist) => {
-        console.log(artist.id, artist.name);
-        artistMain.push(artist);
+        artistMain.push(artist)
 
         if (artistMain.length === artistIds.length) {
-          showArtists(artistMain);
+          showArtists(artistMain)
         }
       })
-      .catch((err) => console.log("ERRORE:", err));
-  });
+      .catch((err) => console.log("ERRORE:", err))
+  })
 
   const showArtists = function (artists) {
-    const slide1 = document.getElementById("artist-slide-1");
-    const slide2 = document.getElementById("artist-slide-2");
+    const container = document.getElementById("artist-container")
 
-    slide1.innerHTML = "";
-    slide2.innerHTML = "";
+    container.innerHTML = ""
 
-    artists.forEach((artist, index) => {
+    artists.forEach((artist) => {
       const card = `
-      <div class="col-6 col-md-3 col-lg-3 col-xl-2 ">
-        <div class="card bg-dark text-light border-0 p-3 text-center align-items-center"  onclick="loadArtistPage(${artist.id})">
-          
-          <img 
-            src="${artist.picture_medium}" 
-            class="rounded-circle mb-3 img-fluid"
-            style="width: 100px; height: auto; object-fit: cover;"
-          >
+      <div 
+        class="card bg-dark text-light border-0 p-3 text-center flex-shrink-0"
+        style="width: 150px; cursor: pointer;"
+        onclick="loadArtistPage(${artist.id})"
+      >
+        <img 
+          src="${artist.picture_medium}" 
+          class="rounded-circle mb-3 mx-auto"
+          style="width: 100px; height: 100px; object-fit: cover;"
+        >
 
-          <p class="mb-1">${artist.name}</p>
-          <small class="text-secondary">Artista</small>
-        </div>
+        <p class="mb-1">${artist.name}</p>
+        <small class="text-secondary">Artista</small>
       </div>
-    `;
+    `
+      console.log("ARTISTI:", artists.length)
 
-      if (index < 6) {
-        slide1.innerHTML += card;
-      } else {
-        slide2.innerHTML += card;
-      }
-    });
-  };
-});
+      container.innerHTML += card
+    })
+  }
+})
