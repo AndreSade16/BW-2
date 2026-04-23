9bc1e0306b0c48d50bc7a4b415ab69bdbd1c2088
Andrea Saderi <andrea.saderi16@gmail.com>
2026-04-23 14:00:38 +0200

    implements search function on navbar search input

--- index-album.js
+++ index-album.js
@@ -1,6 +1,89 @@
 const albumApiLink = "https://striveschool-api.herokuapp.com/api/deezer/album/";
 const main = document.getElementById("main");
 const albumProva = document.getElementById("album-prova");
+const searchInput = document.getElementById("search-input");
+const searchDropdown = document.getElementById("search-dropdown");
+
+// LOGICA SEARCH NAVBAR
+
+if (searchInput) {
+  searchInput.addEventListener("focus", () => {
+    searchDropdown.classList.remove("d-none");
+  });
+
+  searchInput.addEventListener("input", () => {
+    const searchApiLink =
+      "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
+    const trimmedValue = searchInput.value.trim();
+    if (trimmedValue.length > 0) {
+      searchDropdown.classList.remove("d-none");
+      fetch(searchApiLink + trimmedValue)
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            throw new Error("Can't search right now...");
+          }
+        })
+        .then((obj) => {
+          searchDropdown.innerHTML = "";
+          console.log(obj.data);
+          let dropdownHTML;
+          obj.data.forEach((result) => {
+            const { album, id, title, type, artist } = result;
+            const cover = album.cover_small;
+            const albumTitle = album.title;
+            dropdownHTML += `
+            <div id="result-${id}" class="dropdown-item text-light py-2 px-3 d-flex gap-2" type="button">
+                <img src="${cover}" alt="${albumTitle}-cover" style="max-height: 50px">
+                <div class="d-flex flex-column justify-content-center gap-2">
+                  <p class="text-capitalize fw-semibold text-light fs-6 m-0" style="height: 1rem">
+                  ${title}
+                  </p>
+                  <div class="d-flex gap-2 text-body-tertiary">
+                    <p class="fw-semibold fs-6 text-capitalize m-0">
+                        ${type}
+                    </p>
+                    <p class="fw-semibold fs-6 m-0">•</p>
+                    <p class="fw-semibold fs-6 m-0">
+                        ${artist.name}
+                    </p>
+                  </div>
+                </div>
+            </div>
+            `;
+          });
+          searchDropdown.innerHTML = dropdownHTML;
+          obj.data.forEach((result) => {
+            const searchResultDiv = document.getElementById(
+              `result-${result.id}`,
+            );
+            searchResultDiv.addEventListener("click", () => {
+              playAudio(result);
+            });
+          });
+        })
+        .catch((err) => console.log("Failed to fetch", err));
+    } else {
+      searchDropdown.classList.add("d-none");
+    }
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!searchInput.contains(e.target) && !searchDropdown.contains(e.target)) {
+      searchDropdown.classList.add("d-none");
+    }
+  });
+
+  searchInput.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      searchDropdown.classList.add("d-none");
+      searchInput.blur();
+    }
+  });
+}
+
+//
 
 const showHomePage = () => {
   main.scrollTo({
